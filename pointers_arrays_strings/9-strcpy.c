d6300569b5a54bb8399b885363da6484d3901aac
Holberton <holberton@Holbertons-iMac.local>
2023-03-01 14:39:18 -0500

    strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - Write a function that copies the string pointed
+ * @dest: Return value: the pointer
+ * @src:  function parameters
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+while (src[i])
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
