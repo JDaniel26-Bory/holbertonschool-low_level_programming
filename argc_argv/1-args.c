c41fd504599d886f86c3aa809fda6c447f5b8698
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-03-08 11:27:53 -0500

    Silence is argument carried out by other means

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function
+ * @argc: count parameters
+ * @argv: string parameters
+ * Return: nothing;
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
