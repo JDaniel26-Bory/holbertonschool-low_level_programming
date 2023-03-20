4476bc0ccfc8526044859a494689f4a153afbb31
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-03-20 15:30:26 -0700

    Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog´s basic info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the owner of dog
+ * Description - description large
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+#endif
