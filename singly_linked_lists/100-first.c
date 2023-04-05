f495fbb4837b18602a4831e1b602355fe82679d6
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-04-05 15:19:55 -0500

    Write a function that prints

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,12 +1,10 @@
-#include "lists.h"
-
+#include <stdio.h>
 /**
- * before_main - print a message before the main function is execute.
- *
- * Return: nothing.
+ * premain - function that executes before main
+ * __attribute__ -  alias for another symbol
+ * constructor - function called before execution of main
  */
-
-void before_main(void)
+void __attribute__ ((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
